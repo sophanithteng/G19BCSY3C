3f44ed3e5c91075837f8c1b32f1ffa6d40a065f3
sophanithteng <tengsophanith@gmail.com>
2026-07-26 14:15:20 +0700

    seccion 3.7

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
+# composer install
+# wait $!
+# npm install
+# wait $!
 php artisan key:generate
-
-echo "Waiting 20 seconds for MySQL to initialize..."
-sleep 20
-
-echo "Running migrations..."
+wait $!
 php artisan migrate
+wait $!
 
-echo "Starting Laravel server..."
-php artisan serve --host=0.0.0.0 --port=8000
+exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
